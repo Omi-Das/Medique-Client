2d21bb91bef3b1ddd6c93e2f2c6bf739cc2de4d1
Omi-Das <omi06133@gmail.com>
2026-05-17 21:54:48 +0600

    next.config,js updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  turbopack: {
+    // এটি আপনার নেক্সট জেএস অ্যাপের সঠিক রুট ডিরেক্টরি নির্দিষ্ট করে দিবে
+    root: __dirname, 
+  },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
